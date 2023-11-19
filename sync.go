21f8c2f899b5b85bf20bcffd5bd9c3d248f04d82
Mitar <mitar.git@tnode.com>
2023-11-19 18:26:37 +0100

    Add SyncVar.LoadContext.

--- sync.go
+++ sync.go
@@ -1,6 +1,7 @@
 package x
 
 import (
+	"context"
 	"sync"
 
 	"gitlab.com/tozd/go/errors"
@@ -31,6 +32,31 @@ func (w *SyncVar[T]) Load() T { //nolint:ireturn
 		w.cond.Wait()
 	}
 	return *w.v
+}
+
+// LoadContext is similar to Load, but it stops waiting if ctx gets cancelled,
+// returning an error in that case.
+func (w *SyncVar[T]) LoadContext(ctx context.Context) (T, errors.E) { //nolint:ireturn
+	w.cond.L.Lock()
+	defer w.cond.L.Unlock()
+
+	// This is based on example for context.AfterFunc from the context package.
+	// See comments there for explanation how it works and why.
+	stop := context.AfterFunc(ctx, func() {
+		w.cond.L.Lock()
+		defer w.cond.L.Unlock()
+		w.cond.Broadcast()
+	})
+	defer stop()
+
+	for w.v == nil {
+		w.cond.Wait()
+		if ctx.Err() != nil {
+			return *new(T), errors.WithStack(ctx.Err())
+		}
+	}
+
+	return *w.v, nil
 }
 
 // Store stores the value in the SyncVar and unblocks
--- sync_test.go
+++ sync_test.go
@@ -1,6 +1,7 @@
 package x_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,4 +34,69 @@ func TestSyncVar(t *testing.T) {
 	assert.NoError(t, err)
 	errE = v.Store(1)
 	assert.ErrorIs(t, errE, x.ErrSyncVarAlreadyStored)
+}
+
+func TestSyncVarContext(t *testing.T) {
+	t.Parallel()
+
+	v := x.NewSyncVar[int]()
+
+	g := errgroup.Group{}
+	g.Go(func() error {
+		v, err := v.LoadContext(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 1, v)
+		return nil
+	})
+	g.Go(func() error {
+		v, err := v.LoadContext(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 1, v)
+		return nil
+	})
+	g.Go(func() error {
+		v, err := v.LoadContext(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, 1, v)
+		return nil
+	})
+	errE := v.Store(1)
+	assert.NoError(t, errE, "% -+#.1v", errE)
+	err := g.Wait()
+	assert.NoError(t, err)
+	errE = v.Store(1)
+	assert.ErrorIs(t, errE, x.ErrSyncVarAlreadyStored)
+}
+
+func TestSyncVarContextCancel(t *testing.T) {
+	t.Parallel()
+
+	v := x.NewSyncVar[int]()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	g := errgroup.Group{}
+	g.Go(func() error {
+		v, err := v.LoadContext(ctx)
+		assert.ErrorIs(t, err, context.Canceled)
+		assert.Equal(t, 0, v)
+		return nil
+	})
+	g.Go(func() error {
+		v, err := v.LoadContext(ctx)
+		assert.ErrorIs(t, err, context.Canceled)
+		assert.Equal(t, 0, v)
+		return nil
+	})
+	g.Go(func() error {
+		v, err := v.LoadContext(ctx)
+		assert.ErrorIs(t, err, context.Canceled)
+		assert.Equal(t, 0, v)
+		return nil
+	})
+
+	cancel()
+
+	err := g.Wait()
+	assert.NoError(t, err)
 }
