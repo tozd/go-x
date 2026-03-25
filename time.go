27311bf9bec077b4d5551480183cfd42929dcccb
Mitar <mitar.git@tnode.com>
2026-03-25 10:43:42 +0100

    Add TimeFromFloat64.

--- time.go
+++ time.go
@@ -2,7 +2,14 @@ package x
 
 import "time"
 
-// TimeToFloat64 returns the float64 representation of a time.Time.
+// TimeToFloat64 returns the float64 representation of a time.Time,
+// as seconds since the Unix epoch.
 func TimeToFloat64(t time.Time) float64 {
 	return float64(t.Unix()) + float64(t.Nanosecond())/1e9
+}
+
+// TimeFromFloat64 converts float64 seconds since the Unix epoch
+// to time.Time.
+func TimeFromFloat64(t float64) time.Time {
+	return time.Unix(int64(t), int64(t*1e9)%1e9) //nolint:mnd
 }
